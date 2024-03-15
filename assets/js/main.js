63bdb386c548251432cc146a1b40e3e5957f3128
Lucas Schünemann <88516015+lucasschunemann@users.noreply.github.com>
2024-03-15 08:42:56 -0300

    Alterações gerais

--- assets/js/main.js
+++ assets/js/main.js
@@ -19,54 +19,14 @@ if(navClose){
     })
 }
 
-/*=============== REMOVE MENU MOBILE ===============*/
-const navLink = document.querySelectorAll('.nav__link')
-
-const linkAction = () =>{
-    const navMenu = document.getElementById('nav-menu')
-    // When we click on each nav__link, we remove the show-menu class
-    navMenu.classList.remove('show-menu')
+/*=============== CHANGE BACKGROUND HEADER ===============*/
+const bgHeader = () =>{
+   const header = document.getElementById('header')
+   // When the scroll is greater than 50 viewport height, add the scroll-header class to the header tag
+   this.scrollY >= 50 ? header.classList.add('bg-header') 
+                      : header.classList.remove('bg-header')
 }
-navLink.forEach(n => n.addEventListener('click', linkAction))
-
-/*=============== SHADOW HEADER ===============*/
-const shadowHeader = () =>{
-    const header = document.getElementById('header')
-    // When the scroll is greater than 50 viewport height, add the shadow-header class to the header tag
-    this.scrollY >= 50 ? header.classList.add('shadow-header') 
-                       : header.classList.remove('shadow-header')
-}
-window.addEventListener('scroll', shadowHeader)
-
-/*=============== SHOW SCROLL UP ===============*/ 
-const scrollUp = () =>{
-	const scrollUp = document.getElementById('scroll-up')
-    // When the scroll is higher than 350 viewport height, add the show-scroll class to the a tag with the scrollup class
-	this.scrollY >= 350 ? scrollUp.classList.add('show-scroll')
-						: scrollUp.classList.remove('show-scroll')
-}
-window.addEventListener('scroll', scrollUp)
-
-/*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
-const sections = document.querySelectorAll('section[id]')
-    
-const scrollActive = () =>{
-  	const scrollDown = window.scrollY
-
-	sections.forEach(current =>{
-		const sectionHeight = current.offsetHeight,
-			  sectionTop = current.offsetTop - 58,
-			  sectionId = current.getAttribute('id'),
-			  sectionsClass = document.querySelector('.nav__menu a[href*=' + sectionId + ']')
-
-		if(scrollDown > sectionTop && scrollDown <= sectionTop + sectionHeight){
-			sectionsClass.classList.add('active-link')
-		}else{
-			sectionsClass.classList.remove('active-link')
-		}                                                    
-	})
-}
-window.addEventListener('scroll', scrollActive)
+window.addEventListener('scroll', bgHeader)
 
 /*=============== DARK LIGHT THEME ===============*/ 
 const themeButton = document.getElementById('theme-button')
@@ -97,18 +57,3 @@ themeButton.addEventListener('click', () => {
     localStorage.setItem('selected-theme', getCurrentTheme())
     localStorage.setItem('selected-icon', getCurrentIcon())
 })
-
-/*=============== SCROLL REVEAL ANIMATION ===============*/
-const sr = ScrollReveal({
-    origin: 'top',
-    distance: '60px',
-    duration: 2500,
-    delay: 400,
-    // reset: true, // Animations repeat
-})
-
-sr.reveal(`.home__data, .join__container, .footer`)
-sr.reveal(`.home__img`, {origin: 'bottom'})
-sr.reveal(`.enjoy__card, .popular__card`, {interval: 100})
-sr.reveal(`.about__data`, {origin: 'right'})
-sr.reveal(`.about__img`, {origin: 'left'})
